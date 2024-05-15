9eb33522eb274f2f5548149943f53fcb82ba8d96
Ekmid <brotherekmid@gmail.com>
2024-05-15 19:58:19 +0300

    Connected Supabase

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
+"use client"
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import amazonLogo from '../public/amazon-logo-2.webp'
 import { BiCart } from "react-icons/bi";
 import { CgSearch } from "react-icons/cg";
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const itemList = [
     'All',
@@ -15,6 +17,12 @@ const itemList = [
 ]
 
 const Header = () => {
+    const [query, setQuery] = useState<string>("");
+    const router = useRouter()
+    const searchHandler = () => {
+        router.push(`/search/${query}`)
+    }
+
     return (
         <>
             <div className="bg-[#131921] text-white py-1">
@@ -24,8 +32,14 @@ const Header = () => {
                     </div>
 
                     <div className="flex items-center w-[60%]">
-                        <input className='w-full p-2 rounded-l-md outline-none text-black' type="text" placeholder='Search Amazon.in' />
-                        <div className='bg-[#FEBD69] p-2 rounded-r-md'>
+                        <input 
+                        value={query}
+                        onChange={(e) =>setQuery(e.target.value)}
+                        type="text"
+                        className='w-full p-2 rounded-l-md outline-none text-black'  placeholder='Search Amazon.in' />
+                        <div
+                        onClick={searchHandler}
+                        className='bg-[#FEBD69] p-2 rounded-r-md'>
                             <CgSearch className='text-black' size={'24px'} />
                         </div>
                     </div>
@@ -60,7 +74,7 @@ const Header = () => {
                     {
                         itemList.map((link, idx) => {
                             return (
-                                <Link href={'/${link}'} className='mx-2 hover:border border border-transparent hover:border-white p-2'>
+                                <Link key={idx} href={`/${link}`} className='mx-2 hover:border border border-transparent hover:border-white p-2'>
                                     {link}
                                 </Link>
                             )
